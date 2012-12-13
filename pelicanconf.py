f9915a16b5a199bc81b3816f60065bab2eaada2b
lw <luwei@barfoo>
2012-12-13 16:06:57 +0800

    add pelicanconf.py

---
+++ pelicanconf.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'luw2007'
+TWITTER_USERNAME = u'luw2007'
+
+SITENAME = u'luw2007-blog <em>DEV<em>'
+SITETITLE = u'luw2007 blog Dev - Blogging about programming'
+SITEDESCRIPTION = u"luw2007 Developer - Blogging about programming "
+SITEURL = 'http://luw2007.github.com'
+
+DEFAULT_LANG = u'zh'
+TIMEZONE = 'Asia/Shanghai'
+DEFAULT_DATE_FORMAT = '%m-%d-%Y'
+
+PATH = ''
+THEME = 'Just-Read'
+OUTPUT_PATH = '../luw2007.github.com/'
+
+ARTICLE_DIR='archives'
+PAGE_DIR='pages'
+
+FEED_DOMAIN = 'http://luw2007.github.com'
+FEED_RSS = 'feeds/all.rss.xml'
+TAG_FEED_RSS = 'feeds/%s.rss.xml'
+
+MARKUP = 'md'
+
+TAG_CLOUD_STEPS = 8
+DEFAULT_PAGINATION = 9
+
+REVERSE_ARCHIVE_ORDER = True
+
+
+# Blogroll
+#LINKS =  (
+#)
+
+# Social widget
+#SOCIAL = (
+#          ('in 42qu', 'http://n9c_net.42qu.com'),
+#)
+
+
